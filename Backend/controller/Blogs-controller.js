a1a137fd9460293068636273dfeae41adc9f4b46
Zimbini9617 <126578349+Zimbini9617@users.noreply.github.com>
2023-08-23 15:31:12 +0200

    update

--- Backend/controller/Blogs-controller.js
+++ Backend/controller/Blogs-controller.js
@@ -1,5 +1,7 @@
 import Blog from "../model/Blog";
 import User from "../model/User";
+import mongoose from "mongoose";
+
 
 export const getAllBlogs = async (req, res)=>{
   let blogs;
@@ -42,4 +44,65 @@ export const addBlog = async (req, res)=>{
   }
 
   return re.satus(200).json({blog})
+};
+
+export const updateBlog = async (req, res)=>{
+  const blogId = req.params.id;
+  const {title, description} = req.body;
+let blog;
+try {
+  blog = await Blog.findByIdAndUpdate({_id:blogId}, {title, description});
+}catch(err){
+  console.log(err)
 }
+
+if(!blog){
+  return res.status(500).json({meassage: 'Unable to update'})
+}
+return res.status(200).json({ blog });
+};
+
+export const getById = ()=> async (req, res)=>{
+  const blogId = req.params.id;
+  let blog;
+  try {
+    blog = await Blog.findById({_id: blogId})
+  }catch(err){
+    console.log(err)
+  }
+  if (!blog){
+    return res.status(200).json({message: 'Blog not found'});
+  }
+  return res.status(200).json({ blog });
+};
+
+export const deleteBlog = async (req, res)=>{
+  const blogId = req.params.id;
+
+  let blog;
+  try{
+    blog = await blog.findByIdAndRemove({_id: blogId}).populate('user');
+    await blog.user.blogs.pull(blog);
+    await blog.user.save(); 
+  }catch(err){
+    console.log(err)
+  }
+  if(!blog){
+  return res.status(500).json({message: 'Blog not found'})
+  }
+  return res.status(200).json({message: 'Blog is deleted sucessfully'})
+};
+
+export const getByUserId = async (req, res)=>{
+  const userId = req.params.id;
+  let userBlogs;
+  try{
+  userBlogs = await User.findById({_id: userId}).populate('blogs');  
+  }catch(err){
+    console.log(err)
+  }
+  if(!userBlogs){
+    return res.status(404).json({message: 'Blog not found for this user id'});
+  }
+  return res.status(200).json({userBlogs});
+};
--- Backend/route/Blogs-route.js
+++ Backend/route/Blogs-route.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { getAllBlogs, addBlog } from "../controller/Blogs-controller";
+import { getAllBlogs, addBlog, updateBlog, getById, deleteBlog, getByUserId } from "../controller/Blogs-controller";
 
 const blogRouter = express.Router();
 
 blogRouter.get('/', getAllBlogs);
 blogRouter.post('/', addBlog);
+blogRouter.put('/update/:id', updateBlog);
+blogRouter.get('/:id', getById);
+blogRouter.delete('/delete/:id', deleteBlog);
+blogRouter.get('/user/:id', getByUserId)
 
 export default blogRouter;
