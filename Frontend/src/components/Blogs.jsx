d1ce1c73fc8db726e21a6fb77df31873f5ab3242
Zimbini9617 <126578349+Zimbini9617@users.noreply.github.com>
2023-08-28 13:49:04 +0200

    frontend update

--- Frontend/src/components/Blogs.jsx
+++ Frontend/src/components/Blogs.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Card from './Card'; 
+import { useSelector } from 'react-redux';
 
 const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+  const isLoggedIn = useSelector((state)=> state.isLoggedIn);
+  const sendRequest = async ()=> {
+    const res = await axios.get('http://localhost:5000/api/blog');
+    const data = await res.data;
+    return data;
+  };
+
+  useEffect(()=> {
+    sendRequest().then((data) => setBlogs(data.blogs));
+  }, []);
+
   return (
-    <div>Blogs</div>
-  )
-}
+    <div className='flex gap-4 flex-wrap'>
+      {blogs && isLoggedIn && blogs.map((blog) =>(
+        <div key={blog._id}>
+          {console.log(blog.user._id)}
+          <Card title={blog.title}
+          descrption={blog.descrption}
+          image={blog.image}
+          id={blog._id}
+          isUser={localStorage.getItem('userId') === blog.user._id} />
+          </div>
+      ))}
+
+    </div>
+  );
+};
 
 export default Blogs
