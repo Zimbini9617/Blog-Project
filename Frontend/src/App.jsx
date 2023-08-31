81a58661e727819df0c768634ab5d7c2d74389cf
Zimbini9617 <126578349+Zimbini9617@users.noreply.github.com>
2023-08-31 14:45:54 +0200

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,49 +6,46 @@ import Home from './pages/Home';
 import Auth from './components/Auth';
 import Blogs from './components/Blogs';
 import BlogDetail from './components/BlogDetail';
-import UserBlog from './components/UserBlog';
+import UserBlogs from './components/UserBlogs';
 import AddBlog from './components/AddBlog';
-import UpdateBlog from './components/UpdateBlog';
+import UpdateBlog from './components/UpdateBlog'
 
-const router = createBrowserRouter([
+const router = createBrowserRouter([{
+element: <Root />,
+errorElement: <ErrorPage />,
+children: [
   {
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: '/auth',
-        element: <Auth />,
-      },
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: '/blogs',
-        element: <Blogs />,
-      },
-
-      {
-        path: '/user-blog/',
-        element: <UserBlog />,
-      },
-      {
-        path: '/blog-detail/:id',
-        element: <BlogDetail />,
-      },
-      {
-        path: '/add-blog',
-        element: <AddBlog />,
-      },
-      {
-        path: '/update-blog/:id',
-        element: <UpdateBlog />,
-      },
-    ],
+   path: '/',
+   element: <Home /> 
   },
-]);
+  {
+    path: '/auth',
+    element: <Auth /> 
+   },
+   {
+    path: '/blogs',
+    element: <Blogs /> 
+   },
+   {
+    path: '/blog-detail/:id',
+    element: <BlogDetail /> 
+   },
+   {
+    path: '/user-blogs',
+    element: <UserBlogs /> 
+   },
+   {
+    path: '/add-blog',
+    element: <AddBlog /> 
+   },
+   {
+    path: '/update-blog',
+    element: <UpdateBlog /> 
+   },
+]
+}]);
 const App = () => {
   return <RouterProvider router={router} />;
-};
+}
 
-export default App;
+export default App
